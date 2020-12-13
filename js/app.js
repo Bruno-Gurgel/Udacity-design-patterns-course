81d29a55a90c735453b4928c7c28a6cc20d9b9dd
Bruno Gurgel <bmg1612@gmail.com>
2020-12-13 14:46:37 -0300

    chore: Refactored the code using Model, Octopus, Views (MOV) ->
    - The content that the user interacts with is in the view object;
    - The data is on the model object;
    - The Octopus handles both objects so that they are independent in the
    code.

--- js/app.js
+++ js/app.js
@@ -1,43 +1,120 @@
-/**
- * Function to show a cat image when the respective link is clicked.
- * @param {string} event The event itself.
- * @async
- * @event
- */
-document.getElementById("cat-list").addEventListener("click", (event) => {
-  event.preventDefault();
-  const images = document.getElementsByTagName("FIGURE");
-  for (const image of images) {
-    document.getElementById("intro-click").style.display = "none";
-    image.style.display = "none";
-    const currentSelection = event.target.id;
-    const imageID = currentSelection.substring(5);
-    document.getElementById(imageID).style.display = "block";
-  }
-});
-
-const jambo = document.getElementById("cat1");
-const theCat = document.getElementById("cat2");
-const pudim = document.getElementById("cat3");
-const lulu = document.getElementById("cat4");
-const milka = document.getElementById("cat5");
-
-/**
- * Function to count the clicks on a cat.
- * @param {string} element The element that is going to listen to the event.
- * @param {string} figcaption Which 'figcaption' element to show the count.
- * @param {string} name The cat's name.
- */
-const catClick = (element, figcaption, name) => {
-  let count = 0;
-  element.addEventListener("click", () => {
-    count++;
-    document.getElementById(figcaption).innerHTML = `${name}: ${count} clicks.`;
-  });
+/* ======= Model ======= */
+const model = {
+  currentCat: null,
+  cats: [
+    {
+      name: "Jambo",
+      count: 0,
+      imagePath: "../img/cat.jpg",
+      imageAlt: "Grey cat with a blue mask",
+      figCaptionID: "fig-caption-jambo",
+    },
+    {
+      name: "The Cat",
+      count: 0,
+      imagePath: "../img/cat_2.jpg",
+      imageAlt: "Yellow sleepy cat",
+      figCaptionID: "fig-caption-cat",
+    },
+    {
+      name: "Pudim",
+      count: 0,
+      imagePath: "../img/cat_3.jpg",
+      imageAlt: "Yellow baby cat showing half face looking suspicious",
+      figCaptionID: "fig-caption-pudim",
+    },
+    {
+      name: "Lulu",
+      count: 0,
+      imagePath: "../img/cat_4.jpg",
+      imageAlt:
+        "Grey cat with a face that looks like he or she wants something",
+      figCaptionID: "fig-caption-lulu",
+    },
+    {
+      name: "Milka",
+      count: 0,
+      imagePath: "../img/cat_5.jpg",
+      imageAlt: "Cat and a pug dog riding a bicycle",
+      figCaptionID: "fig-caption-milka",
+    },
+  ],
 };
 
-catClick(jambo, "figCapOne", "Jambo");
-catClick(theCat, "figCapTwo", "The Cat");
-catClick(pudim, "figCapThree", "Pudim");
-catClick(lulu, "figCapFour", "Lulu");
-catClick(milka, "figCapFive", "Milka");
+/* ======= Octopus ======= */
+const octopus = {
+  init: () => {
+    // Initially, the current cat is the first on the list.
+    model.currentCat = model.cats[0];
+    // Initializing views.
+    catListView.init();
+    catView.init();
+  },
+  getCurrentCat: () => {
+    return model.currentCat;
+  },
+  getCats: () => {
+    return model.cats;
+  },
+  setCurrentCat: (cat) => {
+    model.currentCat = cat;
+  },
+  incrementCounter: () => {
+    model.currentCat.count++;
+    catView.render();
+  },
+};
+
+/* ======= View ======= */
+const catView = {
+  init: function () {
+    // store pointers to our DOM elements for easy access later
+    this.catImageElem = document.getElementById("cat-image");
+    this.figCaption = document.getElementById("fig-caption");
+
+    // on click, increment the current cat's counter
+    this.catImageElem.addEventListener("click", () => {
+      octopus.incrementCounter();
+    });
+
+    // render this view (update the DOM elements with the right values)
+    this.render();
+  },
+  render: function () {
+    const currentCat = octopus.getCurrentCat();
+    this.catImageElem.src = currentCat.imagePath;
+    this.catImageElem.alt = currentCat.imageAlt;
+    this.figCaption.textContent = `${currentCat.name}: ${currentCat.count} clicks!`;
+  },
+};
+
+const catListView = {
+  init: function () {
+    this.catListElem = document.getElementById("cat-list");
+
+    this.render();
+  },
+  render: function () {
+    const cats = octopus.getCats();
+    let listItem = "";
+    // empty the cat list
+    this.catListElem.innerHTML = "";
+    for (const cat of cats) {
+      listItem = document.createElement("li");
+      listItem.textContent = cat.name;
+
+      listItem.addEventListener(
+        "click",
+        ((catCopy) => {
+          return () => {
+            octopus.setCurrentCat(catCopy);
+            catView.render();
+          };
+        })(cat)
+      );
+      this.catListElem.appendChild(listItem);
+    }
+  },
+};
+
+octopus.init();
